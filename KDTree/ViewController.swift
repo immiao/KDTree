82fe437837b2c68b0c69fb510933b02e00fe00ab
immiao <miaokaixiang@gmail.com>
2017-02-10 00:28:15 -0500

    Initial Commit

---
+++ KDTree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KDTree
+//
+//  Created by mac on 2/10/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
